b20978ada1becae875f8c81053f2d84186b8bfb2
Sidney Piper <sidlapip@gmail.com>
2025-05-11 18:10:02 +0200

    added forward controller

---
+++ backend/src/main/java/dev/saltyyy/portfolio/controllers/ForwardController.java
@@ -0,0 +1,12 @@
+package dev.saltyyy.portfolio.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ForwardController {
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
